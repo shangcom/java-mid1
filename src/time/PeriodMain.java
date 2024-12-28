5026962d31946f1c22d7c932ea8c091fbbf086e0
shangcom <dndbs91@gmail.com>
2024-12-28 15:44:38 +0900

    김영한의 실전 자바 - 중급 1편
    섹션7 - 날짜와 시간
    
    기계 중심의 시간 - Instant
    
    완료

--- src/time/PeriodMain.java
+++ src/time/PeriodMain.java
@@ -1,2 +1,34 @@
-package time;public class PeriodMain {
+package time;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/*
+두 날짜 사이의 간격을 년, 월, 일 단위로 나타낸다.
+ */
+public class PeriodMain {
+    public static void main(String[] args) {
+
+        /*
+         of(): 특정 기간을 지정해서 Period를 생성한다.
+         of(년, 월, 일)
+         ofDays()
+         ofMonths()
+         ofYears()
+         */
+        Period period = Period.ofDays(10); //10일
+        System.out.println("period = " + period);
+
+        // 계산에 사용
+        LocalDate currentDate = LocalDate.of(2030, 1, 1);
+        LocalDate plusDate = currentDate.plus(period);
+        System.out.println("currentDate = " + currentDate);
+        System.out.println("plusDate = " + plusDate);
+
+        // getMonths() : 기간 차이
+        LocalDate startDate = LocalDate.of(2023, 1, 1);
+        LocalDate endDate = LocalDate.of(2023, 4, 2);
+        Period between = Period.between(startDate, endDate);
+        System.out.println("기간 : " + between.getMonths() + "개월 " + between.getDays() + "일");
+    }
 }
